90b8403a65563955bc90e9353b098948e3baa231
Darrien Huntley <dhuntleypro@gmail.com>
2020-03-22 00:16:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,14 @@
 
 board = [" #{"|"}" , " "," "," "," "," "," "," "," "]
 
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
 def display_board(board)
   puts board
 end
